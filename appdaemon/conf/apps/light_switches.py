c1a93108de53d3a3570e4dbe7ca88331fe123c0f
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-08-30 23:32:02 +0200

    add initial appdaemon setup

---
+++ appdaemon/conf/apps/light_switches.py
@@ -0,0 +1,22 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class LightSwitches(hass.Hass):
+
+  def initialize(self):
+    self.listen_state(self.toggle_bath, entity="sensor.bath_switch_action", attribute="action")
+    self.listen_state(self.toggle_living, entity="sensor.living_switch_action", attribute="action")
+
+  def toggle_bath(self, entity, attribute, old, new, kwargs):
+    self.log(f'{entity}: {new}')
+    if new == "off":
+      self.toggle("light.bath_light_01_light")
+      self.toggle("light.bath_light_02_light")
+      self.toggle("light.bath_light_03_light")
+      self.toggle("light.bath_light_04_light")
+    if new == "on":
+      self.toggle("light.bath_mirror_light_light")
+
+  def toggle_living(self, entity, attribute, old, new, kwargs):
+    self.log(f'{entity}: {new}')
+    if new == "on":
+      self.toggle("light.living_light_ceiling_light")
