80f5066390a252bae274037850ace7c00c911004
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-09-13 20:15:49 +0200

    fix blind open/close and add sunrise auto-open

--- appdaemon/conf/apps/sleep_automation.py
+++ appdaemon/conf/apps/sleep_automation.py
@@ -5,13 +5,14 @@ class SleepAutomation(hass.Hass):
   def initialize(self):
     self.listen_state(self.open_close_blinds, "sensor.sleep_blinds_ctrl_01_action", attribute="action")
     self.listen_state(self.open_close_blinds, "sensor.sleep_blinds_ctrl_02_action", attribute="action")
+    self.run_at_sunrise(self.open_blinds, offset=0)
 
   def open_close_blinds(self, entity, attribute, old, new, kwargs):
     self.log(f'"{entity}" fired [action="{new}"]')
-    if new == "open":
+    if new == "close":
       self.call_service("cover/close_cover", entity_id="cover.sleep_blinds_left_cover")
       self.call_service("cover/close_cover", entity_id="cover.sleep_blinds_right_cover")
-    if new == "close":
+    if new == "open":
       self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_left_cover")
       self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_right_cover")
 
