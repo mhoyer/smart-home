b5671b9172cede57e1c959ea00454cc4db8c9faa
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-12-19 17:53:24 +0100

    improve ./restart.sh

--- restart.sh
+++ restart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 docker-compose down
-sleep 5
+sleep 2
 
 docker-compose up -d mosquitto
 sleep 1
 docker-compose up -d zigbee2mqtt
-sleep 2
-docker-compose up -d homeassistant
 sleep 5
+docker-compose up -d homeassistant
+sleep 8
 docker-compose up -d appdaemon
 
