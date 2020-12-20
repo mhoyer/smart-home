00b8331dcfe95c98455d0a7def6b0be4c649b29a
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-12-20 10:46:44 +0100

    add turn on/off delay for bath room lights

--- appdaemon/conf/apps/bath_room.py
+++ appdaemon/conf/apps/bath_room.py
@@ -9,22 +9,29 @@ class BathRoomAutomation(hass.Hass):
     "light.bath_light_04"
   ]
 
-  ambient_lights = ["light.bath_mirror_light"]
+  ambient_light = "light.bath_mirror_light"
 
   def initialize(self):
     self.listen_state(self.on_light_switch_press, "sensor.bath_switch_action", attribute="action")
 
   def on_light_switch_press(self, entity, attribute, old, new, kwargs):
     if new == "off":
-      action = self.turn_on
       if self.get_state(self.ceil_lights[0]) == "on":
-        action = self.turn_off
-
-      list(map(action, self.ceil_lights))
-
+        self.turn_lights_off()
+      else:
+        self.turn_lights_on()
     elif new == "on":
-      action = self.turn_on
-      if self.get_state(self.ambient_lights[0]) == "on":
-        action = self.turn_off
+      if self.get_state(self.ambient_light) == "on":
+        self.turn_off(self.ambient_light)
+      else:
+        self.turn_on(self.ambient_light)
 
-      list(map(action, self.ambient_lights))
+  def turn_lights_on(self):
+    for light in self.ceil_lights:
+      self.turn_on(light)
+      time.sleep(0.2)
+
+  def turn_lights_off(self):
+    for light in self.ceil_lights:
+      self.turn_off(light)
+      time.sleep(0.2)
