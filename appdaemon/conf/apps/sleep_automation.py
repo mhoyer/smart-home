51c79246a86b1138d6a36fd5758441b2d3e4ba53
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-12-20 09:30:45 +0100

    enable sunrise optimized blinds opening algo

--- appdaemon/conf/apps/sleep_automation.py
+++ appdaemon/conf/apps/sleep_automation.py
@@ -1,21 +1,63 @@
 import appdaemon.plugins.hass.hassapi as hass
+from datetime import datetime, timedelta
 
 class SleepAutomation(hass.Hass):
 
   def initialize(self):
-    self.listen_state(self.open_close_blinds, "sensor.sleep_blinds_ctrl_01_action", attribute="action")
-    self.listen_state(self.open_close_blinds, "sensor.sleep_blinds_ctrl_02_action", attribute="action")
-    self.run_at_sunrise(self.open_blinds, offset=-1800)
+    self.listen_state(self.on_switch_triggered, "sensor.sleep_blinds_ctrl_01_action", attribute="action")
+    self.listen_state(self.on_switch_triggered, "sensor.sleep_blinds_ctrl_02_action", attribute="action")
 
-  def open_close_blinds(self, entity, attribute, old, new, kwargs):
+    self.run_daily(self.daily_blind_opening, "04:00:00")
+
+  async def on_switch_triggered(self, entity, attribute, old, new, kwargs):
     self.log(f'"{entity}" fired [action="{new}"]')
     if new == "close":
-      self.call_service("cover/close_cover", entity_id="cover.sleep_blinds_left")
-      self.call_service("cover/close_cover", entity_id="cover.sleep_blinds_right")
+      await self.close_blinds()
     if new == "open":
-      self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_left")
-      self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_right")
+      await self.open_blinds()
 
-  def open_blinds(self, kwargs):
-    self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_left")
-    self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_right")
+  async def open_blinds(self):
+    await self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_left")
+    await self.sleep(1)
+    await self.call_service("cover/open_cover", entity_id="cover.sleep_blinds_right")
+
+  async def close_blinds(self):
+    await self.call_service("cover/close_cover", entity_id="cover.sleep_blinds_right")
+    await self.sleep(1)
+    await self.call_service("cover/close_cover", entity_id="cover.sleep_blinds_left")
+
+  work_day_factor = 1.2
+  weekend_factor = 1.4
+  def daily_blind_opening(self):
+    today = self.date()
+    today_is_workday = today.weekday() < 5
+
+    sunrise_today = self.sunrise()
+    if sunrise_today.day > today.day:
+      # in case we already had sunrise today
+      sunrise_today = sunrise_today - timedelta(1)
+
+    # See: https://www.timeanddate.com/sun/germany/leipzig
+    min_sunrise = datetime(today.year, today.month, today.day, 4, 53)
+    max_sunrise = datetime(today.year, today.month, today.day, 8, 15)
+
+    avg_work_day_wakeup_time = datetime(today.year, today.month, today.day, 6, 45)
+    avg_weekend_wakeup_time = datetime(today.year, today.month, today.day, 8, 30)
+
+    work_day_wakeup_time = sunrise_today + (avg_work_day_wakeup_time - sunrise_today) / self.work_day_factor
+    weekend_wakeup_time = sunrise_today + (avg_weekend_wakeup_time - sunrise_today) / self.weekend_factor
+
+    self.log(f'Sunrise: min={min_sunrise} today={sunrise_today} max={max_sunrise}')
+
+    self.log(f'Work day min (summer): {(min_sunrise + (avg_work_day_wakeup_time - min_sunrise) / self.work_day_factor).time()}')
+    self.log(f'Work day now:          {work_day_wakeup_time.time()}')
+    self.log(f'Work day max (winter): {(max_sunrise + (avg_work_day_wakeup_time - max_sunrise) / self.work_day_factor).time()}\n')
+
+    self.log(f'Weekend min (summer):  {(min_sunrise + (avg_weekend_wakeup_time - min_sunrise) / self.weekend_factor).time()}')
+    self.log(f'Weekend now:           {weekend_wakeup_time.time()}')
+    self.log(f'Weekend max (winter):  {(max_sunrise + (avg_weekend_wakeup_time - max_sunrise) / self.weekend_factor).time()}\n')
+
+    if today_is_workday:
+      self.run_daily(self.open_blinds, work_day_wakeup_time)
+    else:
+      self.run_daily(self.open_blinds, weekend_wakeup_time)
