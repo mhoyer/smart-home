9c193af96c5adf58ecdc3522305f00dad2398b00
Marcel Hoyer <mhoyer@pixelplastic.de>
2020-12-20 10:42:46 +0100

    add delay for hall ligths on/off

--- appdaemon/conf/apps/hall.py
+++ appdaemon/conf/apps/hall.py
@@ -1,4 +1,5 @@
 import appdaemon.plugins.hass.hassapi as hass
+import time
 
 class HallAutomation(hass.Hass):
 
@@ -15,9 +16,21 @@ class HallAutomation(hass.Hass):
   def on_light_switch_press(self, entity, attribute, old, new, kwargs):
     if new != "on": return
 
-    action = self.turn_off if self.get_state(self.lights[0]) == "on" else self.turn_on
-    list(map(action, self.lights))
+    if self.get_state(self.lights[0]) == "on":
+      self.turn_lights_off()
+    else:
+      self.turn_lights_on()
 
   def on_main_door_contact(self, entity, attribute, old, new, kwargs):
     action = self.turn_off if new else self.turn_on
     list(map(action, self.lights))
+
+  def turn_lights_on(self):
+    for light in self.lights:
+      self.turn_on(light)
+      time.sleep(0.2)
+
+  def turn_lights_off(self):
+    for light in self.lights:
+      self.turn_off(light)
+      time.sleep(0.2)
