647204598455db9dcfd79dc9df68fd638861a9d3
Marcel Hoyer <mhoyer@pixelplastic.de>
2021-02-19 19:21:36 +0100

    extend movie_mode according to fan control

--- appdaemon/conf/apps/living_room.py
+++ appdaemon/conf/apps/living_room.py
@@ -31,14 +31,28 @@ class LivingRoomAutomation(hass.Hass, mqtt.Mqtt):
   def on_movie_mode_change(self, entity, attribute, old, new, kwargs):
     if new == "on":
       self.mqtt_publish("hushboxctrl/movie_mode", "on")
+      self.set_state("input_boolean.appdaemon_fan_mode", state="on")
+      self.set_state("input_number.appdaemon_fan_speed", state=24)
       self.turn_off(self.ceiling_light)
       time.sleep(0.5)
       self.turn_off(self.basket_light)
     else:
       self.mqtt_publish("hushboxctrl/movie_mode", "off")
+      self.set_state("input_boolean.appdaemon_fan_mode", state="on")
+      self.set_state("input_number.appdaemon_fan_speed", state=28)
+      self.run_in(self.slow_down_fan, 60)
+      self.run_in(self.turn_off_fan, 150)
       self.turn_on(self.basket_light)
       time.sleep(0.5)
       self.turn_on(self.power_switch_fairy_lights)
+
+  def slow_down_fan(self, *args, **kwargs):
+    self.set_state("input_number.appdaemon_fan_speed", state=24)
+
+  def turn_off_fan(self, *args, **kwargs):
+    self.set_state("input_number.appdaemon_fan_speed", state=23)
+    self.set_state("input_boolean.appdaemon_fan_mode", state="off")
+
 
   def on_fan_mode_change(self, entity, attribute, old, new, kwargs):
     if new == "on":
