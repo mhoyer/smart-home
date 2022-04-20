cdec08247fa3d3404d3b930ab2bc16c6e68d1b38
Marcel Hoyer <mhoyer@pixelplastic.de>
2022-04-20 07:46:55 +0200

    add systemd service support

---
+++ install-systemd-svc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Create a systemd service that autostarts & manages a docker-compose instance in the current directory
+# by Uli Köhler - https://techoverflow.net
+# Licensed as CC0 1.0 Universal
+SERVICENAME=docker__$(basename $(pwd))
+
+if [ "$(whoami)" != "root" ]; then
+  echo "Sorry, you are not root."
+  exit 1
+fi
+
+
+echo "Creating systemd service... /etc/systemd/system/${SERVICENAME}.service"
+
+# Create systemd service file
+cat >/etc/systemd/system/$SERVICENAME.service <<EOF
+[Unit]
+Description=$SERVICENAME
+Requires=docker.service
+After=docker.service
+
+[Service]
+Type=simple
+Restart=Always
+TimeoutSec=600
+User=root
+Group=docker
+WorkingDirectory=$(pwd)
+# Shutdown container (if running) when unit is started
+ExecStartPre=$(which docker-compose) --no-ansi -f docker-compose.yml down
+# Start container when unit is started
+ExecStart=$(pwd)/startup.sh -d
+# Stop container when unit is stopped
+ExecStop=$(which docker-compose) --no-ansi -f docker-compose.yml down
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Enabling systemd service $SERVICENAME"
+systemctl enable $SERVICENAME.service
+
+echo "Starting $SERVICENAME"
+systemctl start $SERVICENAME.service
+
--- startup.sh
+++ startup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-docker-compose up -d mosquitto
-sleep 1
-docker-compose up -d zigbee2mqtt
-sleep 2
-docker-compose up -d homeassistant
-sleep 20
-docker-compose up -d appdaemon
+docker-compose --no-ansi up -d mosquitto
 
+sleep 1
+docker-compose --no-ansi up -d zigbee2mqtt
+
+sleep 2
+docker-compose --no-ansi up -d homeassistant
+
+sleep 60
+docker-compose --no-ansi up -d appdaemon
+
+if [[ "$1" == "-d" ]]; then
+  echo "Entering the endless sleep loop. Check the logs of all containers for further information"
+
+  while true; do
+    sleep 60
+  done
+fi
