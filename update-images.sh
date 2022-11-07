2925c0c40152151e08c911d034d15adf92ebc17e
Marcel Hoyer <mhoyer@pixelplastic.de>
2022-11-07 14:25:58 +0100

    rework ./update-images.sh to actually work as expected

--- update-images.sh
+++ update-images.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
 # getting docker tags, see: https://stackoverflow.com/a/39454426
-# semver sorting, see: https://stackoverflow.com/a/63027058 
+# semver sorting, see: https://stackoverflow.com/a/63027058
 
 function fetch_tags() {
   image="$1"
-  tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${image}/tags -O - \
-    | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' \
-    | tr '}' '\n' \
-    | awk -F: '{print $3}' \
-    | grep -E '^[.0-9]+$' \
+  default_pattern='^[0-9]+\.[0-9]+$'
+  pattern="${2:-$default_pattern}"
+  docker_response=$(wget -q https://registry.hub.docker.com/v2/repositories/${image}/tags?page_size=30 -O -)
+  all_tags=$(
+    echo "${docker_response}" \
+    | jq -r '.results[].name'
+  )
+  if [ -z "${all_tags}"]; then
+    echo "${docker_response}"
+    echo
+    echo "Unable to parse JSON response above from https://registry.hub.docker.com/v1/repositories/${image}/tags."
+    exit 1
+  fi
+
+  matching_tags=$(
+    echo "${all_tags}" \
+    | grep -E "$pattern" \
     | sort -t "." -k1,1n -k2,2n -k3,3n
   )
-  latest_tag=$(echo "$tags" | tail -n1)
+  latest_tag=$(echo "$matching_tags" | tail -n1)
 
-  echo "found $1:$latest_tag"
+  if [ -z "$latest_tag" ]; then
+    echo "${all_tags}"
+    echo
+    echo "Unable to find a matching tag from all tags found  above from https://registry.hub.docker.com/v1/repositories/${image}/tags."
+    exit 1
+  fi
+
+  echo "found $1:$latest_tag (matching the pattern of '$pattern')"
   sed -i "s|image: ${1}:.*|image: ${1}:${latest_tag}|g" docker-compose.yml
 }
 
-fetch_tags koenkk/zigbee2mqtt
-fetch_tags homeassistant/raspberrypi4-homeassistant
+fetch_tags koenkk/zigbee2mqtt '^[0-9]+\.[0-9]+\.[0-9]+$'
+fetch_tags homeassistant/raspberrypi4-homeassistant '^[0-9]+\.[0-9]+\.[0-9]+$'
 
 git diff docker-compose.yml
 
